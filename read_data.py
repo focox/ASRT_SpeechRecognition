c65919bb6274ccdaa384cafcba4fc2545b11ef01
yiang.zhu <yiang.zhu@sohan.hk>
2019-01-07 18:49:10 +0800

    get_data

--- read_data.py
+++ read_data.py
@@ -6,18 +6,58 @@ import os
 import numpy as np
 from general_function.file_wav import *
 import random
+import linecache
 
 class DataSpeech():
 
     def __init__(self, datalinkpath):
         self.train_list, self.cv_list, self.test_list = self.get_data_list(datalinkpath)
-        print(self.train_list, self.cv_list, self.test_list)
+        self.syllable_set = self.read_syllable_dict()
+
+
+    def get_samples(self, data_type):
+        pass
+
+    def syllable2number(self, char):
+        """
+        transform syllable into number that is numbered by the index of char in the pinyin list.
+        :param char: syllable
+        :return: the index of char in the pinyin list.
+        """
+        return self.syllable_set.index(char)
+
+    def read_syllable_dict(self):
+        """
+        read the pinyin file from pinyin dict
+        :return: pinyin list that only consists of pinyin including tone.
+        """
+        syllable_dict_path = os.path.join('.', 'dict.txt')  # this operation can be compatible on any system platform.
+        with open(syllable_dict_path, mode='r') as f:
+            syllable_list = f.readlines()
+        syllable_set = []
+        for i in syllable_list:
+            if i != '':
+                char = i.split('\t')
+                syllable_set.append(char[0])
+        syllable_set.append('_')  # append the character '_' at the end of original list.
+        return syllable_set
+
+    @staticmethod
+    def get_data(data_list):
+        for syllable_link, wav_link in data_list:
+            
+            with open(syllable_link, mode='r') as f:
+                syllable_path = f.readlines()
+            with open(wav_link, mode='r') as f:
+                wav_link = f.readlines()
+
+
 
     @staticmethod
     def get_data_list(datalinkpath):
         """
         get all paths of links that point all real paths of dataset, and return the train_link, cv_link, test_link.
-        :param datalinkpath: the path of link of dataset
+        :param datalinkpath: the path of link of dataset.
         :return:[[path of train_syllable, path of train_wav], []]
                 [[path of cv_syllable, path of cv_wav], []]
                 [[path of test_syllable, path of test_wav], []]
